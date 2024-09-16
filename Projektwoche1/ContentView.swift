63dbbd91d4dc4ca4dfc5e4bceddb90e018ff6001
jakub.zwierz <jakub.zwierz@web.de>
2024-09-16 12:51:12 +0200

    Initial commit

--- Projektwoche1/ContentView.swift
+++ Projektwoche1/ContentView.swift
@@ -7,6 +7,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Test")
         }
         .padding()
     }
